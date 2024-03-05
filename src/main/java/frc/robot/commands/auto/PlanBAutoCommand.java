5b57eb50142306cacc16706252dfeab6eb4275d3
Quentin Field <q@quentinfield.com>
2024-03-05 18:26:35 -0500

    WIP

---
+++ src/main/java/frc/robot/commands/auto/PlanBAutoCommand.java
@@ -0,0 +1,46 @@
+package frc.robot.commands.auto;
+
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.auto.stubs.FakeScoreAmpCommand;
+import frc.robot.commands.swervedrive.DriveDistanceCommand;
+import frc.robot.commands.swervedrive.DriveRobotOrientedCommand;
+import frc.robot.commands.swervedrive.DriveToPositionCommand;
+import frc.robot.commands.swervedrive.ResetOdometryCommand;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
+import frc.robot.subsystems.vision.HughVisionSubsystem;
+
+import static frc.robot.Constants.BotTarget.BLUE_AMP;
+import static frc.robot.Constants.UsefulPoses.SCORE_BLUE_AMP;
+import static frc.robot.Constants.UsefulPoses.SCORE_RED_AMP;
+
+public class PlanBAutoCommand extends SequentialCommandGroup {
+
+    public PlanBAutoCommand(SwerveSubsystem swerve, HughVisionSubsystem hugh) {
+
+        Pose2d blueFinishPose = new Pose2d(4, 7.7, new Rotation2d(90));
+        Pose2d redFinishPose  = new Pose2d(12.54, 7.4, new Rotation2d());
+        //TODO: configure starting pose translation
+        Pose2d blueStartPose = new Pose2d(new Translation2d(), Rotation2d.fromDegrees(270));
+        Pose2d redStartPose = new Pose2d(new Translation2d(), Rotation2d.fromDegrees(270));
+
+        //Start this auto in corner.
+        addCommands(new LogMessageCommand("Starting Auto"));
+        addCommands(new ResetOdometryCommand(swerve, blueStartPose, redStartPose));
+
+        addCommands(new DriveToPositionCommand(swerve, SCORE_BLUE_AMP, SCORE_RED_AMP, 1));
+        addCommands(new DriveRobotOrientedCommand(swerve, new Translation2d(-.1, 0), new Rotation2d()));
+        addCommands(new DriveRobotOrientedCommand(swerve, new Translation2d(.1, 0), new Rotation2d()));
+        addCommands(new DriveToPositionCommand(swerve, blueFinishPose, redFinishPose));
+
+        addCommands(new LogMessageCommand("Auto Complete"));
+
+        }
+}
+
+
+// 34.5 x 36.5
