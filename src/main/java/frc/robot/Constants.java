514d7393f6457a7c4e4985533ee2298988ab8e5b
Tony Field <tony@kingsway.digital>
2024-03-06 17:45:07 -0500

    set measured hold values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -467,16 +467,14 @@ public final class Constants {
         /**
          * Amount of output required to hold the Aim Pivot when the Aim is parallel to the ground
          */
-
-        public static final double      MAX_AIM_HOLD                       = 0;
+        public static final double      MAX_AIM_HOLD                       = 0.03;
 
 
         /**
          * Amount of output required to hold the Link Pivot when the Link and Aim are parallel to
          * the ground
          */
-
-        public static final double      MAX_LINK_HOLD                      = 0;
+        public static final double      MAX_LINK_HOLD                      = 0.04;
     }
 
     public static final class ClimbConstants {
