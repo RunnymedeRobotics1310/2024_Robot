928db8614255894c5ef40a811bfa577878823463
Runnymede Driverstation <1310.runnymede.robotics@gmail.com>
2024-03-09 13:26:47 -0500

    add bracket

--- src/main/java/frc/robot/commands/auto/Score1SpeakerAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/Score1SpeakerAutoCommand.java
@@ -6,7 +6,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.ManualShootCommand;
 import frc.robot.commands.swervedrive.DriveToPositionCommand;
-import frc.robot.commands.swervedrive.RotateToTargetCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
 import frc.robot.subsystems.vision.HughVisionSubsystem;
@@ -15,8 +14,8 @@ public class Score1SpeakerAutoCommand extends SequentialCommandGroup {
 
     public Score1SpeakerAutoCommand(SwerveSubsystem swerve, ArmSubsystem arm, HughVisionSubsystem hugh) {
 
-        Pose2d blueFinishPose = new Pose2d(4, 1.5, new Rotation2d());
-        Pose2d redFinishPose  = new Pose2d(12.54, 1.8, new Rotation2d());
+        Pose2d blueFinishPose  = new Pose2d(4, 1.5, new Rotation2d());
+        Pose2d redFinishPose   = new Pose2d(12.54, 1.8, new Rotation2d());
 
         Pose2d blueTransitPose = new Pose2d(2, 3, new Rotation2d());
         Pose2d redTransitPose  = new Pose2d(10.54, 3, new Rotation2d());
@@ -30,11 +29,11 @@ public class Score1SpeakerAutoCommand extends SequentialCommandGroup {
 
         // IMPORTANT: line up with speaker
 
-        //addCommands(RotateToTargetCommand.createRotateToSpeakerCommand(swerve, hugh));
+        // addCommands(RotateToTargetCommand.createRotateToSpeakerCommand(swerve, hugh));
         addCommands(new ManualShootCommand(arm));
 
         /* Exit Zone */
-        addCommands(new DriveToPositionCommand(swerve, blueTransitPose, redTransitPose);
+        addCommands(new DriveToPositionCommand(swerve, blueTransitPose, redTransitPose));
         addCommands(new DriveToPositionCommand(swerve, blueFinishPose, redFinishPose));
 
         // tell people we're done
