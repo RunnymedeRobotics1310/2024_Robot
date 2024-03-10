834d6026a76b24e5664dc00361d572c8d0a6ee7e
Tony Field <tony@kingsway.digital>
2024-03-10 14:39:45 -0400

    restore hold, fix simpleampositioncmd

--- src/main/java/frc/robot/commands/arm/SimpleAmpPositionCommand.java
+++ src/main/java/frc/robot/commands/arm/SimpleAmpPositionCommand.java
@@ -10,25 +10,17 @@ public class SimpleAmpPositionCommand extends ArmBaseCommand {
 
     @Override
     public void execute() {
-        if (driveToArmPosition(Constants.ArmConstants.SHOOT_AMP_ARM_POSITION, Constants.ArmConstants.DEFAULT_LINK_TOLERANCE_DEG,
-            Constants.ArmConstants.DEFAULT_AIM_TOLERANCE_DEG)) {
-            armSubsystem.setAimPivotSpeed(0.02);
-        }
+        driveToArmPosition(Constants.ArmConstants.SHOOT_AMP_ARM_POSITION, Constants.ArmConstants.DEFAULT_LINK_TOLERANCE_DEG,
+                Constants.ArmConstants.DEFAULT_AIM_TOLERANCE_DEG)
     }
 
     @Override
     public boolean isFinished() {
-        // return driveToArmPosition(Constants.ArmConstants.SHOOT_AMP_ARM_POSITION,
-        // Constants.ArmConstants.DEFAULT_LINK_TOLERANCE_DEG,
-        // Constants.ArmConstants.DEFAULT_AIM_TOLERANCE_DEG);
-
-        // button set to whileTrue
         return false;
     }
 
     @Override
     public void end(boolean interrupted) {
         super.end(interrupted);
-        armSubsystem.stop();
     }
 }
