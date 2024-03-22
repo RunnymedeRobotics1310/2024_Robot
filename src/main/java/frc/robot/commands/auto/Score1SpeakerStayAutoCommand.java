d474e9eb451b1710829ba2ccd9a79edf4492a532
Tony Field <tony@kingsway.digital>
2024-03-21 22:43:31 -0400

    Verify Score1StayAutoCommand

--- src/main/java/frc/robot/commands/auto/Score1SpeakerStayAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/Score1SpeakerStayAutoCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.commands.auto;
 
-
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.arm.ShootCommand;
@@ -11,20 +10,15 @@ import frc.robot.subsystems.vision.HughVisionSubsystem;
 
 public class Score1SpeakerStayAutoCommand extends SequentialCommandGroup {
 
-    public Score1SpeakerStayAutoCommand(SwerveSubsystem swerve, ArmSubsystem arm, HughVisionSubsystem hugh, LightingSubsystem lighting, double delay) {
+    public Score1SpeakerStayAutoCommand(SwerveSubsystem swerve, ArmSubsystem arm, HughVisionSubsystem hugh,
+        LightingSubsystem lighting, double delay) {
 
 
         addCommands(new LogMessageCommand("Starting Auto"));
         addCommands(new WaitCommand(delay));
-        /* ***AUTO PATTERN*** */
-
-        /* Note 1 */
 
         // IMPORTANT: line up with speaker
-
-        // addCommands(RotateToTargetCommand.createRotateToSpeakerCommand(swerve, hugh));
         addCommands(new ShootCommand(arm, lighting));
-
 
         // tell people we're done
         addCommands(new LogMessageCommand("Auto Complete"));
