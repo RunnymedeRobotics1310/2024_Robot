3e0b4ecca72b76a181feb17e6a171c65e6f3658d
Tony Field <tony@kingsway.digital>
2024-03-22 01:02:11 -0400

    Add clone of Score3Speaker called ScoreLoadedWolverineBarnum

---
+++ src/main/java/frc/robot/commands/auto/ScoreLoadedWolverineBarnum.java
@@ -0,0 +1,54 @@
+package frc.robot.commands.auto;
+
+import static frc.robot.Constants.FieldConstants.BLUE_BARNUM;
+import static frc.robot.Constants.FieldConstants.RED_BARNUM;
+import static frc.robot.Constants.UsefulPoses.IN_FRONT_OF_WOLVERINE_BLUE;
+import static frc.robot.Constants.UsefulPoses.IN_FRONT_OF_WOLVERINE_RED;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.arm.CompactCommand;
+import frc.robot.commands.arm.ShootCommand;
+import frc.robot.commands.arm.ShootSpeakerFromPodiumCommand;
+import frc.robot.commands.arm.StartIntakeCommand;
+import frc.robot.commands.swervedrive.*;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.lighting.LightingSubsystem;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
+import frc.robot.subsystems.vision.HughVisionSubsystem;
+import frc.robot.subsystems.vision.JackmanVisionSubsystem;
+
+public class ScoreLoadedWolverineBarnum extends SequentialCommandGroup {
+
+    public ScoreLoadedWolverineBarnum(SwerveSubsystem swerve, ArmSubsystem armSubsystem, HughVisionSubsystem hugh,
+        JackmanVisionSubsystem jackman, LightingSubsystem lighting, double delay) {
+
+        // start
+        addCommands(new LogMessageCommand("Starting Auto"));
+        addCommands(new WaitCommand(delay));
+
+        // score loaded
+        addCommands(new ShootCommand(armSubsystem, lighting));
+
+        // score wolverine
+        addCommands(new DriveToPositionCommand(swerve, IN_FRONT_OF_WOLVERINE_BLUE, IN_FRONT_OF_WOLVERINE_RED));
+        addCommands(
+            new StartIntakeCommand(armSubsystem, lighting)
+                .deadlineWith(new SimpleDriveRobotOrientedCommand(swerve, 1, 0, 0, 1.5)));
+        addCommands(new SimpleDriveRobotOrientedCommand(swerve, -1.0, 0, 0, 0.4));
+        addCommands(RotateToTargetCommand.createRotateToSpeakerCommand(swerve, hugh));
+        addCommands(new ShootSpeakerFromPodiumCommand(armSubsystem, lighting));
+
+        // score barnum
+        addCommands(new CompactCommand(armSubsystem)
+            .alongWith(new RotateToLocationCommand(swerve, BLUE_BARNUM, RED_BARNUM)));
+        addCommands(new StartIntakeCommand(armSubsystem, lighting)
+            .deadlineWith(new DriveToPositionFacingCommand(swerve, BLUE_BARNUM, RED_BARNUM)));
+        addCommands(RotateToTargetCommand.createRotateToSpeakerCommand(swerve, hugh));
+        addCommands(new ShootSpeakerFromPodiumCommand(armSubsystem, lighting));
+
+        // end
+        addCommands(new LogMessageCommand("Auto Complete"));
+
+    }
+}
--- src/main/java/frc/robot/commands/operator/OperatorInput.java
+++ src/main/java/frc/robot/commands/operator/OperatorInput.java
@@ -318,6 +318,8 @@ public class OperatorInput {
         autoPatternChooser.addOption("Loaded + Wolverine (Speaker)", Constants.AutoConstants.AutoPattern.SCORE_LOADED_WOLVERINE);
         autoPatternChooser.addOption("2 Speaker Vision", Constants.AutoConstants.AutoPattern.SCORE_2_SPEAKER_VISION);
         autoPatternChooser.addOption("3 Speaker", Constants.AutoConstants.AutoPattern.SCORE_3_SPEAKER);
+        autoPatternChooser.addOption("Loaded + Wolverine + Barnum (Speaker",
+            Constants.AutoConstants.AutoPattern.SCORE_LOADED_WOLVERINE_BARNUM);
         autoPatternChooser.addOption("4 Speaker", Constants.AutoConstants.AutoPattern.SCORE_4_SPEAKER);
 
 
@@ -365,6 +367,7 @@ public class OperatorInput {
         case THE_DOUBLE_DOWN -> new TheDoubleDown(drive, arm, lighting, delay);
         case SCORE_LOADED_WOLVERINE -> new ScoreLoadedWolverine(drive, arm, hugh, jackman, lighting, delay);
         case SCORE_2_SPEAKER_VISION -> new Score2SpeakerVisualAutoCommand(drive, arm, hugh, jackman, lighting, delay);
+        case SCORE_LOADED_WOLVERINE_BARNUM -> new ScoreLoadedWolverineBarnum(drive, arm, hugh, jackman, lighting, delay);
         case SCORE_3_SPEAKER -> new Score3SpeakerAutoCommand(drive, arm, hugh, jackman, lighting, delay);
         case SCORE_4_SPEAKER -> new Score4SpeakerAutoCommand(drive, arm, hugh, jackman, lighting, delay);
         default -> new InstantCommand();
