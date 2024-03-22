133ee873f2e6ab8db3533e2f8e1b2244f5952070
Tony Field <tony@kingsway.digital>
2024-03-21 20:51:33 -0400

    new drive in exit zone auto

--- src/main/java/frc/robot/commands/auto/ExitZoneAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/ExitZoneAutoCommand.java
@@ -6,11 +6,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.swervedrive.DriveRobotOrientedCommand;
-import frc.robot.commands.swervedrive.DriveToPositionCommand;
-import frc.robot.commands.swervedrive.ResetOdometryCommand;
-import frc.robot.commands.swervedrive.RotateToTargetCommand;
-import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.commands.swervedrive.*;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
 
 public class ExitZoneAutoCommand extends SequentialCommandGroup {
@@ -24,7 +20,7 @@ public class ExitZoneAutoCommand extends SequentialCommandGroup {
 
         /* Exit Zone */
         addCommands(new ResetOdometryCommand(swerve, new Pose2d(new Translation2d(), new Rotation2d())));
-        addCommands(new DriveRobotOrientedCommand(swerve, new Translation2d(3, 0), new Rotation2d()));
+        addCommands(new SimpleDriveRobotOrientedCommand(swerve, 1, 0, 0, 3));
 
         // tell people we're done
         addCommands(new LogMessageCommand("Auto Complete"));
