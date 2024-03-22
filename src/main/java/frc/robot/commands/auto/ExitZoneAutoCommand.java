a9cacac8db990aa7633bbde4543ffcd81631a876
Tony Field <tony@kingsway.digital>
2024-03-21 22:40:24 -0400

    Plan C auto

--- src/main/java/frc/robot/commands/auto/ExitZoneAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/ExitZoneAutoCommand.java
@@ -9,15 +9,11 @@ public class ExitZoneAutoCommand extends SequentialCommandGroup {
 
     public ExitZoneAutoCommand(SwerveSubsystem swerve, double delay) {
 
-
         addCommands(new LogMessageCommand("Starting Auto"));
         addCommands(new WaitCommand(delay));
-        /* ***AUTO PATTERN*** */
 
-        /* Exit Zone */
         addCommands(new SimpleDriveRobotOrientedCommand(swerve, 1, 0, 0, 3));
 
-        // tell people we're done
         addCommands(new LogMessageCommand("Auto Complete"));
     }
 }
