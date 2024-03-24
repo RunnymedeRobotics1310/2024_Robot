e1e01923100dde7a008cdf0f2e49608d373389e0
Runnymede Driverstation <1310.runnymede.robotics@gmail.com>
2024-03-24 09:42:35 -0400

    floor trap flop shot tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -598,7 +598,7 @@ public final class Constants {
         public static final int        RIGHT_CLIMB_MOTOR_CAN_ADDRESS = 60;
         public static final int        LEFT_CLIMB_MOTOR_CAN_ADDRESS  = 61;
 
-        public static final int CLIMB_READY_FOR_TRAP_SHOT = 15;
+        public static final int        CLIMB_READY_FOR_TRAP_SHOT     = 7;
 
         public static final double     MAX_ROBOT_LIFT_SPEED          = 1;
         public static final double     RAISE_CLIMBERS_SPEED          = .75;
