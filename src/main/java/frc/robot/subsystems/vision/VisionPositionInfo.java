b4068d6651d23aa055b8b37889ba9090e43d5a72
Tony Field <tony@kingsway.digital>
2024-03-23 23:31:09 -0400

    Undo changes

--- src/main/java/frc/robot/subsystems/vision/VisionPositionInfo.java
+++ src/main/java/frc/robot/subsystems/vision/VisionPositionInfo.java
@@ -1,14 +1,17 @@
 package frc.robot.subsystems.vision;
 
+import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 
-public record VisionPositionInfo(Pose2d pose, double latencyMillis, PoseConfidence poseConfidence) {
+public record VisionPositionInfo(Pose2d pose, double latencySeconds, Matrix<N3, N1> deviation) {
     public String toString() {
         return String.format("(%.2f,%.2f)m %.0fd %.0fms ",
             pose.getTranslation().getX(),
             pose.getTranslation().getY(),
             pose.getRotation().getDegrees(),
-            latencyMillis) + poseConfidence;
+            latencySeconds);
     }
 }
 
