9c99a1206c7dc26d34973d1af3a21dbe157faab6
Jeff Zakrzewski <jeff@zakr.ca>
2024-03-30 09:55:10 -0400

    Fix arm angle

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -480,7 +480,7 @@ public final class Constants {
         public static final int         LINK_ABSOLUTE_ENCODER_ANALOG_PORT  = 3;
         // Encoder constants to convert from Volts to Deg
         public static final double      LINK_ABSOLUTE_ENCODER_DEG_PER_VOLT = 49.11;
-        public static final double      LINK_ABSOLUTE_ENCODER_OFFSET_DEG   = -62;
+        public static final double      LINK_ABSOLUTE_ENCODER_OFFSET_DEG   = -63.5;
 
 
         public static final int         LINK_LOWER_LIMIT_SWITCH_DIO_PORT   = 0;
@@ -491,7 +491,7 @@ public final class Constants {
         // Increasing aim offset by 78 to account for a change that happened on Saturday Mar 9. New
         // measurement added March 10th at 11:20am
         // 73 degree offset on Mar 15.
-        public static final double      AIM_ABSOLUTE_ENCODER_OFFSET_DEG    = 14.7;
+        public static final double      AIM_ABSOLUTE_ENCODER_OFFSET_DEG    = 16.2;
 
         public static final int         INTAKE_NOTE_DETECTOR_DIO_PORT      = 1;
 
