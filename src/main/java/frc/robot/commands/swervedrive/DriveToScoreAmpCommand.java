a65d2589432b7c8021ebc43bcb168fb101b31019
Tony Field <tony@kingsway.digital>
2024-03-30 13:57:16 -0400

    Drive to amp auto

--- src/main/java/frc/robot/commands/swervedrive/DriveToScoreAmpCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/DriveToScoreAmpCommand.java
@@ -1,8 +1,6 @@
 package frc.robot.commands.swervedrive;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.Constants;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
@@ -11,42 +9,33 @@ import static frc.robot.RunnymedeUtils.getRunnymedeAlliance;
 
 public class DriveToScoreAmpCommand extends BaseDriveCommand {
 
-    private final Pose2d  bluePose;
-    private final Pose2d  redPose;
-
-    private Translation2d location;
-    private Translation2d nearby;
-    private Rotation2d    heading;
-    private double        speed;
+    private Pose2d nearby;
+    private Pose2d amp;
+    private double speed;
 
     private enum State {
-        MOVE_NEARBY, ROTATING, CLOSE_ALIGNING, DONE
+        MOVE_NEARBY, CLOSE_ALIGNING, DONE
     }
 
     private State state = State.MOVE_NEARBY;
 
     public DriveToScoreAmpCommand(SwerveSubsystem swerve) {
         super(swerve);
-        this.bluePose = Constants.UsefulPoses.SCORE_BLUE_AMP;
-        this.redPose  = Constants.UsefulPoses.SCORE_RED_AMP;
-        this.heading  = null;
-        this.speed    = Constants.Swerve.Chassis.MAX_TRANSLATION_SPEED_MPS;
+        this.speed = Constants.Swerve.Chassis.MAX_TRANSLATION_SPEED_MPS;
     }
 
     @Override
     public void initialize() {
         if (getRunnymedeAlliance() == DriverStation.Alliance.Blue) {
-            location = bluePose.getTranslation();
-            heading  = bluePose.getRotation();
+            this.nearby = Constants.UsefulPoses.PRE_SCORE_BLUE_AMP;
+            this.amp    = Constants.UsefulPoses.SCORE_BLUE_AMP;
         }
         else {
-            location = redPose.getTranslation();
-            heading  = redPose.getRotation();
+            this.nearby = Constants.UsefulPoses.PRE_SCORE_RED_AMP;
+            this.amp    = Constants.UsefulPoses.SCORE_RED_AMP;
         }
-        nearby = location.minus(new Translation2d(0, .75));
-        logCommandStart("desiredPose: " + new Pose2d(location, heading));
         if (isCloseEnough(nearby)) {
-            state = State.ROTATING;
+            state = State.CLOSE_ALIGNING;
         }
         else {
             state = State.MOVE_NEARBY;
@@ -59,19 +48,13 @@ public class DriveToScoreAmpCommand extends BaseDriveCommand {
 
         switch (state) {
         case MOVE_NEARBY:
-            driveToFieldPose(new Pose2d(nearby, heading), speed);
+            driveToFieldPose(nearby, speed);
             if (isCloseEnough(nearby)) {
-                state = State.ROTATING;
-            }
-            break;
-        case ROTATING:
-            driveToFieldPose(new Pose2d(nearby, heading), speed);
-            if (isCloseEnough(heading)) {
                 state = State.CLOSE_ALIGNING;
             }
             break;
         case CLOSE_ALIGNING:
-            driveToFieldPose(new Pose2d(location, heading), speed / 2);
+            driveToFieldPose(amp, speed / 2);
             break;
         }
     }
@@ -84,6 +67,6 @@ public class DriveToScoreAmpCommand extends BaseDriveCommand {
     @Override
     public boolean isFinished() {
         super.isFinished();
-        return isCloseEnough(location) && isCloseEnough(heading);
+        return isCloseEnough(amp);
     }
 }
