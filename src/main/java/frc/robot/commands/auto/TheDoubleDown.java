4cf5f7d18df4eac6447d5e3b1ae679e74aa51d03
Tony Field <tony@kingsway.digital>
2024-03-30 22:15:56 -0400

    Temporary compensation for drifting - 1 degree per second at 1mps during TheDoubleDown.  We should fix the drift, but this is fine for this auto for now.

--- src/main/java/frc/robot/commands/auto/TheDoubleDown.java
+++ src/main/java/frc/robot/commands/auto/TheDoubleDown.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.auto;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.arm.CompactCommand;
@@ -11,6 +12,8 @@ import frc.robot.subsystems.lighting.LightingSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
 
 public class TheDoubleDown extends SequentialCommandGroup {
+
+    private static Rotation2d TEMP_DRIFT_COMPENSATION = Rotation2d.fromDegrees(1);
 
     public TheDoubleDown(SwerveSubsystem swerve, ArmSubsystem armSubsystem, LightingSubsystem lighting, double delay) {
 
@@ -24,7 +27,7 @@ public class TheDoubleDown extends SequentialCommandGroup {
         addCommands(new StartIntakeCommand(armSubsystem, lighting)
             .deadlineWith(new SimpleDriveRobotOrientedCommand(swerve, 1, 0, 0, 3)));
         addCommands(new CompactCommand(armSubsystem));
-        addCommands(new SimpleDriveRobotOrientedCommand(swerve, -1, 0, 0, 3));
+        addCommands(new SimpleDriveRobotOrientedCommand(swerve, -1, 0, TEMP_DRIFT_COMPENSATION.getRadians(), 3));
         addCommands(new ShootCommand(armSubsystem, lighting));
 
         // end
