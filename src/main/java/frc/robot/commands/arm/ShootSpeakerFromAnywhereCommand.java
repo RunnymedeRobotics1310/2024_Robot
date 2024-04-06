d25f2c5ce488375f34f4580c7d462a1b1ebf5913
Runnymede Driverstation <1310.runnymede.robotics@gmail.com>
2024-04-06 11:49:50 -0400

    remove conditional on shoot

--- src/main/java/frc/robot/commands/arm/ShootSpeakerFromAnywhereCommand.java
+++ src/main/java/frc/robot/commands/arm/ShootSpeakerFromAnywhereCommand.java
@@ -91,13 +91,10 @@ public class ShootSpeakerFromAnywhereCommand extends ArmBaseCommand {
 
     private boolean driveArmToCalculatedAngle() {
         // Drive to the arm position at the same time
-        double linkAngle        = ArmConstants.SHOOT_SPEAKER_PODIUM_ARM_POSITION.linkAngle;
-        double distanceToTarget = getDistanceToTarget();
-        double aimAngle         = SpeakerShooterPolynomialAngleCalc.calculateAimAngle(distanceToTarget);
-        if (lastDistanceToTarget >= 2.88 && lastDistanceToTarget < 2.99) {
-            aimAngle += .75;
-        }
-        Constants.ArmPosition armPositionNew = new Constants.ArmPosition(linkAngle, aimAngle);
+        double                linkAngle        = ArmConstants.SHOOT_SPEAKER_PODIUM_ARM_POSITION.linkAngle;
+        double                distanceToTarget = getDistanceToTarget();
+        double                aimAngle         = SpeakerShooterPolynomialAngleCalc.calculateAimAngle(distanceToTarget);
+        Constants.ArmPosition armPositionNew   = new Constants.ArmPosition(linkAngle, aimAngle);
 
         return driveToArmPosition(armPositionNew, 2, 2);
     }
